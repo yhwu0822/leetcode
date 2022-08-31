bd91f38a97eca8b83ece6d198e8ceb9175df0103
yhwu0822 <112546498+yhwu0822@users.noreply.github.com>
2022-08-31 18:28:34 +0800

    Add files via upload

---
+++ 234_Palindrome_Linked_List_o.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        # reverse function
+        def reverse(prev, head):
+            if not head:
+                return prev
+            tmp = head.next
+            head.next = prev
+            return reverse(head, tmp)
+
+        # use slow and fast pointer to get the mid of list
+        sp = head
+        fp = sp
+        while (fp and fp.next):
+            sp = sp.next
+            fp = fp.next.next
+
+        mid = reverse(None, sp)
+
+        # check for palindrome
+        def check(mid, head):
+            if (not mid):
+                return True
+            elif (mid.val == head.val):
+                return check(mid.next, head.next)
+            else:
+                return False
+
+        return check(mid, head)
