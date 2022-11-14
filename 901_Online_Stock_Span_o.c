a4bb5dd5057f064c7f545df094c2f886b7578e2b
yhwu0822 <112546498+yhwu0822@users.noreply.github.com>
2022-11-14 13:31:43 +0800

    Add files via upload

---
+++ 901_Online_Stock_Span_o.c
@@ -0,0 +1,35 @@
+class StockSpanner {
+public:
+    StockSpanner() {
+
+    }
+
+    int next(int price) {
+        int curPrice = price;
+        int curSpan = 1;
+
+        // Compute price span in stock data with monotonic stack
+        while( stack.size() && stack.back().first <= price ){
+
+            auto [prevPrice, prevSpan] = stack.back();
+
+            stack.pop_back();
+
+            // update current price span with history data in stack
+            curSpan += prevSpan;
+        }
+
+        // update latest price quote and price span
+        stack.push_back( pair<int, int>{curPrice, curSpan} );
+
+        return curSpan;
+    }
+private:
+    vector< pair<int, int> > stack;
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
