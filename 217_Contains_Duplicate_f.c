263634502adcac8ecda9c0a4bcdc753743bbea3f
yhwu0822 <112546498+yhwu0822@users.noreply.github.com>
2022-11-28 10:16:12 +0800

    Add files via upload

---
+++ 217_Contains_Duplicate_f.c
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int, int> count;
+        for (int i = 0; i < nums.size(); i++){
+            count[nums[i]]++;
+            if( count[nums[i]] > 1 ){
+                return true;
+            }
+        }
+        return false;
+    }
+};
